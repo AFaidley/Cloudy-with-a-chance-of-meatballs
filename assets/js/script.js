136597fc3b672c6793ab0db4fb4ced823e5db21a
AFaidley <alviva.faidley@gmail.com>
2022-07-24 22:51:33 -0600

    Created for loop for 5day forecast and cleaned up code in js, also added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,85 +12,86 @@ var iconOne = document.getElementById("icon1");
 var cities = [];
 
 function start() {
-    currentWeather.setAttribute("class", "hide");
-    fiveDay.setAttribute("class", "hide");
+  currentWeather.setAttribute("class", "hide");
+  fiveDay.setAttribute("class", "hide");
 }
 
 searchBtn.addEventListener("click", function (event) {
-    // console.log(input.value);
-    event.preventDefault();
+  // console.log(input.value);
+  event.preventDefault();
 
-    fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?q=${input.value}&appid=${apiKey}&units=imperial&appid=2bfd37d5d2aa41c253c3db29c2f30228`
-    )
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${input.value}&appid=${apiKey}&units=imperial&appid=2bfd37d5d2aa41c253c3db29c2f30228`
+  )
+    .then((response) => {
+      //   console.log(response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      // var searchIcon = data.list[0].weather[0].icon;
+      // var searchCity = data.city.name;
+      var searchLat = data.city.coord.lat;
+      var searchLon = data.city.coord.lon;
+      // 5day forecast vars for textContent add later
+      var day1 = moment().add(1, "days").format("l");
+      var day2 = moment().add(2, "days").format("l");
+      var day3 = moment().add(3, "days").format("l");
+      var day4 = moment().add(4, "days").format("l");
+      var day5 = moment().add(5, "days").format("l");
+
+      fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${searchLat}&lon=${searchLon}&appid=${apiKey}&units=imperial`
+      )
         .then((response) => {
-            //   console.log(response);
-            return response.json();
+          //   console.log(response);
+          return response.json();
         })
         .then((data) => {
-              console.log(data);
-            var searchIcon = data.list[0].weather[0].icon;
-            var searchCity = data.city.name;
-            var searchLat = data.city.coord.lat;
-            var searchLon = data.city.coord.lon;
-            // variables for today's forecast
-            var wiconEl = document.createElement('img');
-            var iconToday = document.createElement('td');
-            var tempToday = document.createElement('td');
-            var humdityToday = document.createElement('td');
-            var uviToday = document.createElement('td');
-            var windToday = document.createElement('td');
-            //variables for daily loop
-            var dailyIcon = document.createElement('img');
-            var dailyIcons = document.createElement('td');
-            var dailyTemp = document.createElement('td');
-            var dailyHumdity = document.createElement('td');
-            var dailyWind = document.createElement('td');
-            // variables for 5-day forecast loop
-            var fiveIcons;
-            var fiveTemp;
-            var fiveWind;
-            var fiveHumdity;
-            // variables for dates of 5-day forecast
-            var day1 = moment().add(1, 'days').format('l');
-            var day2 = moment().add(2, 'days').format('l'); 
-            var day3 = moment().add(3, 'days').format('l');
-            var day4 = moment().add(4, 'days').format('l');
-            var day5 = moment().add(5, 'days').format('l');
-            
-            
-            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${searchLat}&lon=${searchLon}&appid=${apiKey}&units=imperial`)
-            .then((response) => {
-                //   console.log(response);
-                return response.json();
-            })
-            .then((data) => {
-                document.getElementById("current-uvindex").textContent="UV Index: " +  data.current.uvi + " ";
-            });
-            // Weather Forecast for Today 
-            document.getElementById("current-pic").src= `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png`;
-            document.getElementById("current-temp").textContent=`Temp: ${data.list[0].main.temp} °F`;
-            document.getElementById("current-wind").textContent=`Wind: ${data.list[0].wind.speed} MPH`;
-            document.getElementById("current-humdity").textContent=`Humidity: ${data.list[0].main.humidity} %`;
-        
-            
-            // Populate row of tables with Dates
-            document.getElementById("day1").textContent = day1;
-            document.getElementById("day2").textContent = day2;
-            document.getElementById("day3").textContent = day3;
-            document.getElementById("day4").textContent = day4;
-            document.getElementById("day5").textContent = day5;
+          document.getElementById("current-uvindex").textContent =
+            "UV Index: " + data.current.uvi + " ";
+        });
+      // Weather Forecast for current day
+      document.getElementById(
+        "current-pic"
+      ).src = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png`;
+      document.getElementById(
+        "current-temp"
+      ).textContent = `Temp: ${data.list[0].main.temp} °F`;
+      document.getElementById(
+        "current-wind"
+      ).textContent = `Wind: ${data.list[0].wind.speed} MPH`;
+      document.getElementById(
+        "current-humdity"
+      ).textContent = `Humidity: ${data.list[0].main.humidity} %`;
 
-            for (var i = 1; i < 6; i++) { 
-                //dynamic variables for 5-day forecast
-                var icons = document.getElementById("icon"+ [i]);
-                trTemp = '#temp'+ [i];
-                trWind = '#wind'+ [i];
-                trHumdity = '#humdity'+ [i];
-                
-               
-            }
-           
-        })});
+      //Add dates of 5 day forecast using the vars above
+      document.getElementById("day1").textContent = day1;
+      document.getElementById("day2").textContent = day2;
+      document.getElementById("day3").textContent = day3;
+      document.getElementById("day4").textContent = day4;
+      document.getElementById("day5").textContent = day5;
+
+      for (var i = 1; i < 6; i++) {
+        //Automatically update the 5day forecast with having to call each day separately
+        var icons = document.getElementById("pic" + [i]);
+        var temp = document.getElementById("temp" + [i]);
+        var wind = document.getElementById("wind" + [i]);
+        var humdity = document.getElementById("humdity" + [i]);
+        // console.log(icons)
+        // console.log(i)
+
+        dailycurrentIcon = data.list[i].weather[0].icon;
+        dailyIcons = `https://openweathermap.org/img/wn/${dailycurrentIcon}.png`;
+        icons.src = dailyIcons;
+
+        temp.textContent = data.list[i].main.temp + " °F";
+
+        wind.textContent = data.list[i].wind.speed + " MPH";
+
+        humdity.textContent = data.list[i].main.humidity + " %";
+      }
+    });
+});
 
 // start();
